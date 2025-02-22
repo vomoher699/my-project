0db6068e7fba94a08b0ddb783ce3e3f63ec2fe0c
vomoher699 <154703523+vomoher699@users.noreply.github.com>
2025-02-22 21:53:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')  # Removed the colon
 def home():  
-    return "Hello World"
+    return "Hello Worl"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8000)
